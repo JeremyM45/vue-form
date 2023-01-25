a46d3470e16a77424e1fdcfe084f95b89b8518f1
JeremyM45 <jeremyvs1945@gmail.com>
2023-01-24 18:23:10 -0800

    add signup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script>
     import Login from './components/Login.vue';
+    import Signup from './components/Signup.vue'
 
     export default {
         name: "App",
         components: {
             Login,
+            Signup
         }
     }
 </script>
 
 <template>
     <div class="h-screen bg-blue-500 flex justify-center items-center">
+        <div class="absolute left-96">
+            <Signup />
+        </div>
         <Login />
     </div>
 </template>
---
+++ src/components/Signup.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        name: "Signup"
+    }
+</script>
+
+<template>
+    <div class="bg-white w-[30rem] h-[40rem]">
+
+    </div>
+</template>
